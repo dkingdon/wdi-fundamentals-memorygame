38cd58e73f3997ac719b8d2a29b8a1820abb7cd2
Daniel Kingdon <dkingdon@Daniels-MBP.hsd1.ca.comcast.net>
2016-10-10 09:42:42 -0700

    added working html and css files and declared card variables

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+/* Declaring card variables */
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
