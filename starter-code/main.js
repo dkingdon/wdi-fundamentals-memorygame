d5d6b25d99d7ed804836f3f60803d4d8cbea713e
Daniel Kingdon <dkingdon@Daniels-MBP.hsd1.ca.comcast.net>
2016-10-14 17:26:09 -0700

    progress through unit 11 assignment. Current step is 7

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,32 +1,43 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
 
-/* Declaring card variables 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
 
+var cardsInPlay = [];
 
-if (cardOne == cardTwo) {
-	alert("You found a match");
-}
-else if (cardThree == cardFour) {
-	alert("You found a match"); 
-}
-else {
-	alert("Sorry, try again")
-}
-*/
 
 // 10.7 step 4-10 Note: had to defer js script in html to make this work.  
 var createCards = function() {
 	var gameBoard = document.getElementById('game-board');
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < cards.length; i++) {
 		var newDiv = document.createElement('div');
 		newDiv.className = "card";
-		gameBoard.appendChild(newDiv)
+		
+		// Unit 11.6 steps 3 & 4
+		newDiv.setAttribute('data-card', cards[i]);
+		newDiv.addEventListener('click', isTwoCards);
+		
+		gameBoard.appendChild(newDiv);
 	}
 }
 createCards();
+
+// Unit 11.6 step 6
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute("data-card"));
+		if (cardsInPlay.length == 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
+}
+
+// Unit 11.6 step 5 
+var isMatch = function(choiceOne, choiceTwo){
+	if (choiceOne == choiceTwo) {
+		alert("You found a match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
 
 
